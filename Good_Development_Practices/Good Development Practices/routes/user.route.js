9f7b088a65ea9263f1cc8dd78a96592d04c183ec
Sandhya85b <sandhya85.b@gmail.com>
2025-02-23 22:03:41 +0530

    Implemented put/users route

--- Good_Development_Practices/Good Development Practices/routes/user.route.js
+++ Good_Development_Practices/Good Development Practices/routes/user.route.js
@@ -13,15 +13,27 @@ userRoute.get("/", async (req, res) => {
     res.status(500).json({ msg: "Error in getting list of all users" });
   }
 });
-userRoute.post("/",async(req,res)=>{
-  try{
-   const newUser=new userModel(req.body)
-   await newUser.save()
-   res.status(201).json({msg:"User created successfull"})
-  }catch(err){
-    res.status(500).json({msg:"Failed to create User"})
+userRoute.post("/", async (req, res) => {
+  try {
+    const newUser = new userModel(req.body);
+    await newUser.save();
+    res.status(201).json({ msg: "User created successfull" });
+  } catch (err) {
+    res.status(500).json({ msg: "Failed to create User" });
   }
-})
+});
 
+userRoute.put("/:id", async (req, res) => {
+  try {
+    const updateUser = await userModel.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    res.status(200).json({ msg: "User updated sucessfull" });
+  } catch (err) {
+    res.status(500).json({ msg: "Failed to update user" });
+  }
+});
 
 module.exports = userRoute;
